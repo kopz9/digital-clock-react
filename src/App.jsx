d5a0bfb23957ec69e264c2721e844d5d1c2265a3
kopz9 <gustavoeduardo438@gmail.com>
2024-02-01 03:08:59 -0300

    feat: Change to 12-hour and 24-hour clock.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import DigitalClock from './DigitalClock'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <DigitalClock />
--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react';
 function DigitalClock(){
 
   const [time, setTime] = useState(new Date());
+  const [is24Hour, setIs24Hour] = useState(false);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -22,14 +23,33 @@ function DigitalClock(){
     return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)}`
   }
 
+  function formatMeridienTime(){
+    let hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();    
+    const meridiem = hours >= 12 ? "PM" : "AM";
+
+    hours = hours % 12 || 12;
+
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`
+
+  }
+
   function padZero(number){
     return (number < 10 ? "0" : "") + number;
+  }
+
+  function changeFormat(){
+    setIs24Hour((prevFormat) => !prevFormat);
   }
 
   return(
     <div className='clock-container'>
       <div className='clock'>
-        <span>{formatTime()}</span>
+      <button className="change-btn" onClick={changeFormat}>Change Format</button>
+      <br></br>
+        <span>{is24Hour ? formatTime() : formatMeridienTime()}</span>
+        <br></br>        
       </div>
     </div>
   )
